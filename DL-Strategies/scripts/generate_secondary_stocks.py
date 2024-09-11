37e3f074ce63f578749fca7229f79d7e1c2c29df
Taliwat <kalpha1865@gmail.com>
2024-09-10 22:06:30 -0400

    optimized features (ntbk), implemented in sec script, revised linreg ntbks with optimized features and reran, still need to comment all.

--- DL-Strategies/scripts/generate_secondary_stocks.py
+++ DL-Strategies/scripts/generate_secondary_stocks.py
@@ -7,7 +7,9 @@ import pandas as pd
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from scripts.utils import load_config
-from statsmodels.tsa.stattools import coint
+from sklearn.feature_selection import RFECV
+from sklearn.linear_model import Ridge
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 
 # core sectors are the divisions that our core stocks below belong to, just going to list them for use later.
@@ -34,23 +36,6 @@ def get_nasdaq100_tickers():
     tables = pd.read_html(url)
     df = tables[4]
     return df['Ticker'].tolist()
-
-# Now from these lists let's filter for just the Technology stocks so we can use later.
-def sec_stocks_filter(tickers, valid_sectors, core_tickers):
-    sec_stocks = []
-    for ticker in tickers:
-        if ticker in core_tickers:
-            continue
-        try:
-            info = yf.Ticker(ticker).info
-            sector = info.get('sector', None)
-            if sector in valid_sectors:
-                sec_stocks.append({
-                    'ticker' : ticker
-                })
-        except Exception as e:
-            print(f"Error processing {ticker}: {e}")
-    return sec_stocks
 
 # Now we will now extract our new features from the pandas-ta library for our technical indicators.
 def calculate_indicators(df, window_sma = 50, window_ema = 50, window_rsi = 14):
@@ -146,104 +131,107 @@ def fill_missing_vals(df):
     
     return df
 
+
 # Bringing it all together, let's use our previous function calls here along with the data settings in our config
 # file to create a new csv dataframe of our secondary stocks to be used later.
 def main(config):
-    max_secondary_stocks = config['yfinance']['max_secondary_stocks']
-    start_date = config['yfinance']['start_date']
-    end_date = config['yfinance']['end_date']
-    core_tickers = config['yfinance']['core_tickers']
+    use_preprocessed = config['yfinance']['use_preprocessed'] # Uses already loaded pre-processed data.
     
-    
-    valid_sectors = CORE_SECTORS
-    
-    sp500_tickers = get_sp500_tickers()
-    nasdaq100_tickers = get_nasdaq100_tickers()
-    all_tickers = list(set(sp500_tickers + nasdaq100_tickers))
-    
-    sec_stocks = sec_stocks_filter(all_tickers, valid_sectors, core_tickers)    
+    # Logic for if the config file has use_preprocessed set to True
+    if use_preprocessed:
+        print("Using pre-processed data.")
+        csv_path = config['yfinance']['csv_paths']['sec_stock_unscaled']
+        df_secondary_stocks = pd.read_csv(csv_path, parse_dates = ['Date'], index_col = 'Date')
+        print(f"Pre-processed data loaded successfully with shape: {df_secondary_stocks.shape}")
         
-    historical_data = {}
-    
-    successful_tickers = 0
-    
-    for stock in sec_stocks:
-        if successful_tickers >= max_secondary_stocks:
-            break
-        
-        try:
-            data = yf.download(stock['ticker'], start = start_date, end = end_date)[['Close', 'Volume', 'Open', 'High', 'Low']]
-            
-            print(f"Columns before renaming for {stock['ticker']}: {data.columns}")
-            
-            if data.empty or 'Close' not in data.columns:
-                print(f"No data or 'Close' column missing for {stock['ticker']}, skipping.")
-                continue
-            
-            data = data.rename(columns = {
-                'Close' : 'Close_sec',
-                'Volume' : 'Volume_sec',
-                'Open' : 'Open_sec',
-                'High' : 'High_sec',
-                'Low' : 'Low_sec',
-            })
-            
-            print(f"Columns after renaming for {stock['ticker']}: {data.columns}")
-            
-            print(f"Downloaded data for {stock['ticker']}: \n{data.head()}")
-            
-            # An important lines here, we will set the active first row to 03-14.  This offsets the initial rolling window from our data entry point of 01-01.
-            data = data[data.index >= '2019-03-14']
-            
-            data = calculate_indicators(data)
-            data = fill_missing_vals(data)
-            
-            if 'Close_sec' not in data.columns:
-                print(f"Close_sec is missing for {stock['ticker']}, skipping composite score.")
-                continue
-            
-            historical_data[stock['ticker']] = data
-            successful_tickers += 1
-            
-        except Exception as e:
-            print(f"Error processing {stock['ticker']}: {e}")
-    
-    if not historical_data:
-        print("No data was successfully downloaded.")
+        # Exclude core stocks from newly generated stocks
+        core_tickers = config['yfinance']['core_tickers']
+        df_secondary_stocks = df_secondary_stocks[~df_secondary_stocks['ticker'].isin(core_tickers)]
         
     else:
+        print("Fetching raw stock data.")
+        max_secondary_stocks = config['yfinance']['max_secondary_stocks']
+        start_date = config['yfinance']['start_date']
+        end_date = config['yfinance']['end_date']
+        core_tickers = config['yfinance']['core_tickers']
+    
+        sp500_tickers = get_sp500_tickers()
+        nasdaq100_tickers = get_nasdaq100_tickers()
+        all_tickers = list(set(sp500_tickers + nasdaq100_tickers))
+    
+        # Filter out core stocks from the new incoming list of secondary stocks if they get generated
+        all_tickers = [ticker for ticker in all_tickers if ticker not in core_tickers]
+    
+        historical_data = {}
+    
+        successful_tickers = 0
+    
+        for ticker in all_tickers:
+            if successful_tickers >= max_secondary_stocks:
+                break
+        
+            try:
+                data = yf.download(ticker, start = start_date, end = end_date)[['Close', 'Volume', 'Open', 'High', 'Low']]
+            
+                if data.empty or 'Close' not in data.columns:
+                    print(f"No data or 'Close' column missing for {ticker}, skipping.")
+                    continue
+            
+                data = data.rename(columns = {
+                    'Close' : 'Close_sec',
+                    'Volume' : 'Volume_sec',
+                    'Open' : 'Open_sec',
+                    'High' : 'High_sec',
+                    'Low' : 'Low_sec',
+                })
+            
+            
+                # An important lines here, we will set the active first row to 03-14.  This offsets the initial rolling window from our data entry point of 01-01.
+                data = data[data.index >= '2019-03-14']
+            
+                data = calculate_indicators(data)
+                data = fill_missing_vals(data)
+            
+                historical_data[ticker] = data
+                successful_tickers += 1
+            
+            except Exception as e:
+                print(f"Error processing {ticker}: {e}")
+    
+        if not historical_data:
+            print("No data was successfully downloaded.")
+        
+            return
+    
         df_secondary_stocks = pd.concat(historical_data.values(), keys = historical_data.keys(), names = ['ticker', 'Date'])
         df_secondary_stocks = df_secondary_stocks.reset_index(level=['ticker', 'Date'])
-        df_secondary_stocks['Date'] = pd.to_datetime(df_secondary_stocks['Date'])
     
-        #sort_indicators = df_secondary_stocks.groupby('ticker').agg({
-        #    'Composite_Score' : 'last'
-        #}).reset_index()
+    # Use the optimized feature list from the config file
+    optimized_features = config['yfinance']['optimized_features']
+    print(f"Optimized features: {optimized_features}")
+        
+    # Calculate the scores for each stock based on optimized features
+    stock_scores = df_secondary_stocks.groupby('ticker')[optimized_features].mean()
+    print(f"Stock scores:\n{stock_scores.head()}")    
     
-        #final_indicators_sorted = sort_indicators.sort_values(by = 'Composite_Score', ascending = False)
-        #top_indicators = final_indicators_sorted.head(max_secondary_stocks)
-        
-        #secondary_stocks_gen = df_secondary_stocks[df_secondary_stocks['ticker'].isin(top_indicators['ticker'])]
-        
-        #print(secondary_stocks_gen.isna().sum())
-        
-        #secondary_stocks_gen.reset_index(drop = True, inplace = True)
-        #secondary_stocks_gen.set_index('Date', inplace = True)
-        
-        #print(secondary_stocks_gen.head())
-        
-        #secondary_stocks_gen.to_csv(config['yfinance']['csv_paths']['secondary_stocks_gen'])
-        
-        # Alternate script until boruta arrives
-        secondary_stocks_gen = df_secondary_stocks
-        print(secondary_stocks_gen.isna().sum())
-        
-        secondary_stocks_gen.reset_index(drop = True, inplace=True)
-        secondary_stocks_gen.set_index('Date', inplace = True)
-        
-        print(secondary_stocks_gen.head())
-        
-        secondary_stocks_gen.to_csv(config['yfinance']['csv_paths']['secondary_stocks_gen'])
+    # Sort the stocks by their scores (using a sum of means across these same optimized features)
+    stock_scores['total_score'] = stock_scores.sum(axis = 1)
+    
+    print(f"Selecting top {config['yfinance']['max_secondary_stocks']} stocks.")
+    top_stocks = stock_scores.nlargest(config['yfinance']['max_secondary_stocks'], 'total_score').index
+    print(f"Top stocks selected:\n{top_stocks}")
+    
+    # Filter the data for the optimized features, but keep ALL original features intact
+    secondary_stocks_gen_filtered = df_secondary_stocks[df_secondary_stocks['ticker'].isin(top_stocks)]
+    
+    print(secondary_stocks_gen_filtered.head())
+    
+    # Save to the appropriate path
+    secondary_stocks_gen_filtered.to_csv(config['yfinance']['csv_paths']['secondary_stocks_gen_filtered'])
+    
+    print(f"Final data saved to{config['yfinance']['csv_paths']['secondary_stocks_gen_filtered']}")
+    print(f"Number of stocks in the final data: {len(secondary_stocks_gen_filtered['ticker'].unique())}")
+    
     
 if __name__ == "__main__":
     config_path = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.path.dirname(__file__),
